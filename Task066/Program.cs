fc2a2c947c8796c5c52a638b5747fdca413315cb
Ildar <akhmilg@gmail.com>
2023-03-30 20:21:05 -0700

    Added Task064, Task066

---
+++ Task066/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое целое положительное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое положительное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int summ = Sum(numberN, numberM);
+if (numberN<0||numberM<0)
+Console.Write("Вводите только положительные числа");
+else
+Console.Write($"Сумма чисел в промежутке от {numberN} до {numberM} равна {summ}");
+
+
+int Sum(int num1, int num2)
+{
+    int max= Math.Max(num1, num2);
+    int min= Math.Min(num1, num2);
+    if (min == max) return min;
+    else return min + Sum(min + 1, max);
+}
+
